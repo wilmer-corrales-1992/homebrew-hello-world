d79883206d5e9cb88df48f184e7c582ae90d5e45
Wilmer Corrales <wilmer.corrales@instructure.com>
2022-12-08 16:23:14 -0600

    Version 0.2.0

--- hello_world.rb
+++ hello_world.rb
@@ -2,23 +2,19 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HelloWorld < Formula
-    desc ""
-    homepage ""
+    desc "Test Formula"
+    homepage "https://github.com/wilmer-corrales-1992/hello_world"
     url "https://github.com/wilmer-corrales-1992/hello_world/archive/refs/tags/v0.0.2.tar.gz"
     sha256 "543e05757934832cea6ef9a776ec5936225ad9bac6b8175422c0d066bff8e462"
-    license ""
+    license "MIT"
   
     # depends_on "cmake" => :build
   
     def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-      system "./configure", *std_configure_args, "--disable-silent-rules"
-      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      bin.install 'hello_world'
     end
   
-    test do
+    # test do
       # `test do` will create, run in and delete a temporary directory.
       #
       # This test will fail and we won't accept that! For Homebrew/homebrew-core
@@ -28,7 +24,7 @@ class HelloWorld < Formula
       #
       # The installed folder is not in the path, so use the entire path to any
       # executables being tested: `system "#{bin}/program", "do", "something"`.
-      system "false"
-    end
+    #   system "false"
+    # end
   end
   
