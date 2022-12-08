73b66e50c8e7895fee9c6d4d0a2d1fc3c5956a87
Wilmer Corrales <wilmer.corrales@instructure.com>
2022-12-08 15:37:55 -0600

    first commit

---
+++ hello_world.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HelloWorld < Formula
+    desc ""
+    homepage ""
+    url "https://github.com/wilmer-corrales-1992/hello_world/archive/refs/tags/v0.0.2.tar.gz"
+    sha256 "543e05757934832cea6ef9a776ec5936225ad9bac6b8175422c0d066bff8e462"
+    license ""
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test hello_world`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
